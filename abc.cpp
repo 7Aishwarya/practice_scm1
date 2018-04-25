6623a799213cfa77f6aca9871ee9ad39c8795f06
demo user <demouser@demo.com>
2018-04-25 15:22:21 +0530

    My last commit in master branch - Aishwarya

--- abc.cpp
+++ abc.cpp
@@ -1,17 +1,17 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-//comment 1
+//comment 1 (By Aishwarya)
 //comment 2
 
 =======
-edited by ishika//comment 1
-edited by ishita//comment 2
+edited by ishika//comment 1 (By Ishika)
+edited by ishita//comment 2 (By Ishita)
 >>>>>>> origin/ishika
 =======
 //comment 1 
 //comment 2
-//comment 3 made at 1 am
+//comment 3 made at 1 am (By Ishika)
 >>>>>>> origin/ishika2
 //comment 1
 //comment 2
@@ -20,5 +20,6 @@ edited by ishita//comment 2
 //comment 1 hi
 //comment 2
 //comment 3 made at 1 am
-//comment 4 "made at 1.42"
+//comment 4 "made at 1.42" (By Garishma)
 >>>>>>> origin/garishma
+//my last comment - Aishwarya
